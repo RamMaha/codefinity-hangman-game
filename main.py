0fba6b947f57bbd0fd30f635a1c53b5ca95c9b0e
RamMaha <>
2026-03-17 20:18:40 +0000

    Completing the Hangman Game

---
+++ main.py
@@ -0,0 +1,26 @@
+# List of words used for the Hangman game
+WORDS = [
+    "python",
+    "hangman",
+    "developer",
+    "console",
+    "project",
+    "function",
+    "integer",
+    "variable"
+]
+
+# Symbol used to represent hidden letters
+HIDDEN_SYMBOL = "_"
+
+# Maximum number of incorrect attempts allowed
+MAX_ATTEMPTS = 6
+
+
+def main():
+    # Entry point for the game
+    pass
+
+
+if __name__ == "__main__":
+    main()
